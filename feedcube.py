15197bbb1d4f64896e22c60cc4d894d4a1b7ca62
jared <jared@revel.in>
2017-10-19 21:10:48 -0400

    Reworked FeedContent, plus added code to update FeedContent and avoid duplicate RSS posts

--- feedcube.py
+++ feedcube.py
@@ -1,21 +1,21 @@
-from flask import Flask, request, render_template, url_for
-from flask_mongoalchemy import MongoAlchemy
-from forms import addfeed, loginform
-from functions import get_fields_from_form
-from xml.dom.minidom import parseString
-from datetime import datetime
-from requests import get
+from datetime import datetime as dt
+from datetime import timedelta
 from re import match
+from xml.dom.minidom import parseString
 
+from flask import Flask, request, render_template
+from flask_mongoalchemy import MongoAlchemy
+from requests import get
+
+from forms import addfeed, loginform
+from functions import get_fields_from_form, xml_to_dict, format_description, df
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-
 app.config['MONGOALCHEMY_DATABASE'] = 'feedcube'
 app.config['MONGOALCHEMY_SERVER_AUTH'] = False
 app.config['MONGOALCHEMY_PORT'] = 27018
-
 
 db = MongoAlchemy(app)
 
@@ -24,14 +24,24 @@ class User(db.Document):
     username = db.StringField(max_length=20)
     password = db.StringField(max_length=20)
     email = db.StringField(max_length=40)
-    date_registered = db.DateTimeField(default=datetime.utcnow())
+    date_registered = db.CreatedField()
 
 
 class Feed(db.Document):
     feed_name = db.StringField(max_length=80)
     feed_location = db.StringField()
-    feed_content = db.ListField(db.TupleField(db.StringField(), db.StringField()))
-    date_added = db.DateTimeField(default=datetime.utcnow())
+    date_added = db.CreatedField()
+    last_updated = db.DateTimeField()
+
+
+class FeedContent(db.Document):
+    for_feed = db.DocumentField(Feed)
+    source_author = db.StringField(max_length=40)
+    source_title = db.StringField(max_length=120)
+    source_location = db.StringField()
+    source_content = db.StringField()
+    source_added = db.DateTimeField()
+    feed_added = db.DateTimeField()
 
 
 class AssignedFeed(db.Document):
@@ -57,15 +67,19 @@ def add_feed():
         if match('https?://(?:[\w-]+\.)+(?:[a-zA-Z]{2,6})/.+', request.form['feed_location']):
             rss_req = get(request.form['feed_location'])
             if rss_req.status_code == 200:
-                feed_content = []
                 rss_xml = parseString(rss_req.content)
                 rss_items = rss_xml.getElementsByTagName('item')
-                for items in list(rss_items)[:10]:
-                    feed_content += [(x.childNodes[0].data, y.childNodes[0].data)
-                                    for x, y in zip(items.getElementsByTagName('title'), items.getElementsByTagName('guid'))]
                 new_feed = Feed(feed_name=request.form['feed_name'],
-                                feed_location=request.form['feed_location'], feed_content=feed_content)
+                                feed_location=request.form['feed_location'], last_updated=dt.utcnow())
                 new_feed.save()
+                for items in list(rss_items):
+                    item_details = xml_to_dict(items)
+                    feed_content = \
+                        FeedContent(for_feed=new_feed, source_author=item_details['dc:creator'],
+                                    source_title=item_details['title'], source_location=item_details['guid'],
+                                    source_content=format_description(item_details['description']),
+                                    source_added=dt.strptime(item_details['pubDate'], df()), feed_added=dt.utcnow())
+                    feed_content.save()
         else:
             print('Invalid URL!')
     form_fields = get_fields_from_form(addfeed.AddFeed())
@@ -74,8 +88,27 @@ def add_feed():
 
 @app.route('/viewfeed/<feed_id>')
 def view_feed(feed_id):
-    requested_feed = Feed.query.filter(Feed.mongo_id == feed_id).one()
-    return render_template('viewfeed.html', feed=requested_feed)
+    feed_obj = Feed.query.filter(Feed.mongo_id == feed_id).one()
+    if feed_obj.last_updated + timedelta(minutes=15) < dt.utcnow():
+        latest_entry = FeedContent.query.descending('source_added').first()
+        rss_req = get(feed_obj.feed_location)
+        if rss_req.status_code == 200:
+            rss_xml = parseString(rss_req.content)
+            rss_items = rss_xml.getElementsByTagName('item')
+            for items in list(rss_items):
+                item_details = xml_to_dict(items)
+                if dt.strptime(item_details['pubDate'], df()) > latest_entry.source_added:
+                    feed_content = \
+                        FeedContent(for_feed=feed_obj, source_author=item_details['dc:creator'],
+                                    source_title=item_details['title'], source_location=item_details['guid'],
+                                    source_content=format_description(item_details['description']),
+                                    source_added=dt.strptime(item_details['pubDate'], df()), feed_added=dt.utcnow())
+                    feed_content.save()
+                else:
+                    break
+    feed_content_obj = FeedContent.query.filter(FeedContent.for_feed == feed_obj)
+    requested_feed_content = feed_content_obj.all()[:10]  # TODO: change to max_links later
+    return render_template('viewfeed.html', feed_content=requested_feed_content)
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,2 +1,17 @@
+from re import sub
+
+
+def df():
+    return '%a, %d %b %Y %H:%M:%S GMT'
+
+
 def get_fields_from_form(form):
     return [field for field in form][1:]
+
+
+def xml_to_dict(element):
+    return dict([(child.tagName, child.childNodes[0].data) for child in element.childNodes if child.nodeType == 1])
+
+
+def format_description(desc, max_chars=500):
+    return ' '.join(sub('<[^>]+(>|$)', '', desc)[:max_chars].split(' ')[:-1])
