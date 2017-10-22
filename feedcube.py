15a935ea9bc37d871fc2b164c50cf3d4bfb00caf
jared <jared@revel.in>
2017-10-22 03:29:36 -0400

    Finished code for assigning feeds to users, now focussed on styling the site

--- feedcube.py
+++ feedcube.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from re import match
 from xml.dom.minidom import parseString
 
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, current_user
 from flask_mongoalchemy import MongoAlchemy
 from passlib.hash import argon2
@@ -81,9 +81,15 @@ def register():
     elif request.method == 'POST':
         register_data = request.form
         if not User.query.filter(User.username == register_data['username']).first():
-            new_user = User(username=register_data['username'], password=argon2.hash(register_data['password']),
-                            email=register_data['email_address'], is_active=True, is_authenticated=False,
-                            is_anonymous=False, date_registered=dt.utcnow())
+            new_user = User(
+                username=register_data['username'],
+                password=argon2.hash(register_data['password']),
+                email=register_data['email_address'],
+                is_active=True,
+                is_authenticated=False,
+                is_anonymous=False,
+                date_registered=dt.utcnow()
+            )
             new_user.save()
             return redirect('/login')
         else:
@@ -102,7 +108,7 @@ def login():
             user = login_request.one()
             login_request.set(User.is_authenticated, True).execute()
             login_user(user)
-            return redirect('/dashboard')
+            return redirect(url_for('dashboard'))
     else:
         return 'Something went wrong!'
 
@@ -112,21 +118,38 @@ def login():
 def add_feed():
     if request.method == 'POST':
         if match('https?://(?:[\w-]+\.)+(?:[a-zA-Z]{2,6})/.+', request.form['feed_location']):
-            rss_req = get(request.form['feed_location'])
-            if rss_req.status_code == 200:
-                rss_xml = parseString(rss_req.content)
-                rss_items = rss_xml.getElementsByTagName('item')
-                new_feed = Feed(feed_name=request.form['feed_name'],
-                                feed_location=request.form['feed_location'], last_updated=dt.utcnow())
-                new_feed.save()
-                for items in list(rss_items):
-                    item_details = xml_to_dict(items)
-                    feed_content = \
-                        FeedContent(for_feed=new_feed, source_author=item_details['dc:creator'],
-                                    source_title=item_details['title'], source_location=item_details['guid'],
-                                    source_content=format_description(item_details['description']),
-                                    source_added=dt.strptime(item_details['pubDate'], df()), feed_added=dt.utcnow())
-                    feed_content.save()
+            if not Feed.query.filter(Feed.feed_location == request.form['feed_location']).all():
+                rss_req = get(request.form['feed_location'])
+                if rss_req.status_code == 200:
+                    rss_xml = parseString(rss_req.content)
+                    rss_items = rss_xml.getElementsByTagName('item')
+                    new_feed = Feed(
+                        feed_name=request.form['feed_name'],
+                        feed_location=request.form['feed_location'],
+                        last_updated=dt.utcnow()
+                    )
+                    new_feed.save()
+                    af_query = AssignedFeed.query.filter(AssignedFeed.assigned_to.mongo_id == current_user.mongo_id)
+                    assign_feed = AssignedFeed(
+                        assigned_to=User.query.filter(User.mongo_id == current_user.mongo_id).one(),
+                        for_feed=new_feed,
+                        display_order=len(af_query.all()) + 1,
+                        max_links=10,
+                        date_assigned=dt.utcnow()
+                    )
+                    assign_feed.save()
+                    for items in list(rss_items):
+                        item_details = xml_to_dict(items)
+                        feed_content = FeedContent(
+                            for_feed=new_feed,
+                            source_author=item_details['dc:creator'],
+                            source_title=item_details['title'],
+                            source_location=item_details['guid'],
+                            source_content=format_description(item_details['description']),
+                            source_added=dt.strptime(item_details['pubDate'], df()),
+                            feed_added=dt.utcnow()
+                        )
+                        feed_content.save()
         else:
             print('Invalid URL!')
     form_fields = get_fields_from_form(addfeed.AddFeed())
@@ -146,28 +169,37 @@ def view_feed(feed_id):
             for items in list(rss_items):
                 item_details = xml_to_dict(items)
                 if dt.strptime(item_details['pubDate'], df()) > latest_entry.source_added:
-                    feed_content = \
-                        FeedContent(for_feed=feed_obj, source_author=item_details['dc:creator'],
-                                    source_title=item_details['title'], source_location=item_details['guid'],
-                                    source_content=format_description(item_details['description']),
-                                    source_added=dt.strptime(item_details['pubDate'], df()), feed_added=dt.utcnow())
+                    feed_content = FeedContent(
+                        for_feed=feed_obj,
+                        source_author=item_details['dc:creator'],
+                        source_title=item_details['title'],
+                        source_location=item_details['guid'],
+                        source_content=format_description(item_details['description']),
+                        source_added=dt.strptime(item_details['pubDate'], df()),
+                        feed_added=dt.utcnow()
+                    )
                     feed_content.save()
                 else:
                     break
     feed_content_obj = FeedContent.query.filter(FeedContent.for_feed.mongo_id == feed_id)
-    requested_feed_content = feed_content_obj.descending('source_added').all()[:10]  # TODO: change to max_links later
+    num_max_links = AssignedFeed.query.filter(
+        AssignedFeed.assigned_to.mongo_id == current_user.mongo_id
+    ).first().max_links
+    requested_feed_content = feed_content_obj.descending('source_added').all()[:int(num_max_links)]
     return render_template('viewfeed.html', feed_content=requested_feed_content)
+
+
+@app.route('/allfeeds')
+def all_feeds():
+    global_feeds = Feed.query.descending('date_added').all()
+    return render_template('allfeeds.html', feeds=global_feeds)
 
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
     all_feeds = AssignedFeed.query.ascending('display_order').all()
-    if all_feeds:
-        print(all_feeds)
-        return 'Dashboard'
-    else:
-        return 'Welcome, %s!' % current_user.username
+    return render_template('dashboard.html', feeds=all_feeds)
 
 
 if __name__ == '__main__':
